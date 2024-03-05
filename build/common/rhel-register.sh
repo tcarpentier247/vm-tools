2d8edf636e8f01bb11941bcd2bb23299d5c6e793
Théo Carpentier <theo.carpentier@opensvc.com>
2024-03-05 10:56:27 +0100

    Add packer rhel9

---
+++ build/common/rhel-register.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -a 
+
+MAJOR_VERSION=$(grep ^VERSION_ID /etc/os-release | awk -F= '{print $2}' | sed -e 's/"//g' | awk -F. '{print $1}')
+OS_ID=$(grep ^ID= /etc/os-release | awk -F= '{print $2}' | sed -e 's/"//g')
+
+echo '==> Attaching Red Hat subscriptions'
+echo '==> Credentials are' ${RHN_ORG} 'and' ${RHN_KEY}
+subscription-manager unregister
+
+subscription-manager register --org ${RHN_ORG} --activationkey ${RHN_KEY}
+
+[[ ${MAJOR_VERSION} -eq 7 ]] && [[ ${OS_ID} == "rhel" ]] && {
+    yum clean all
+    subscription-manager repos --enable rhel-7-server-optional-rpms
+    subscription-manager repos --enable rhel-7-server-extras-rpms
+    yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+    yum repolist
+    exit 0
+}
+
+dnf clean all
+
+subscription-manager repos --enable rhel-${MAJOR_VERSION}-for-x86_64-supplementary-rpms
+subscription-manager repos --enable codeready-builder-for-rhel-${MAJOR_VERSION}-x86_64-rpms
+dnf repolist
+
+echo '==> Subscriptions successfully attached'
+sleep 3600
+exit 0
