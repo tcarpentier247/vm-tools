523c9bbd7b57c616409f7fbeb9b87b5e9e07d5ec
Théo Carpentier <theo.carpentier@opensvc.com>
2024-03-19 10:33:22 +0100

    Add zfs and drbd scripts

--- build/rhel9/scripts/ansible.sh
+++ build/rhel9/scripts/ansible.sh
@@ -4,14 +4,6 @@ echo "--- Begin ansible.sh ---"
 
 export DEBIAN_FRONTEND=noninteractive
 
-# Install Ansible repository.
-dnf update -y
-dnf install -y dnf-utils
-
-# Install Ansible.
-dnf update -y
 dnf install -y ansible
-
-dnf clean all
 
 echo "--- End ansible.sh ---"
