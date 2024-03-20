97bf64d8eed4066aaf92ac3b27d31609cabe5739
Arnaud Veron <arnaud.veron@opensvc.com>
2024-03-20 08:38:04 +0100

    Fix var-file cli option
    * only needed when secrets are passed to packer

--- build/build.sh
+++ build/build.sh
@@ -16,9 +16,11 @@ for img in $DISTRO_IMG
 do
     [[ ! -d $img ]] && continue
     echo "--- building image $img ---"
+    PACKER_OPTS=""
     cd $img && {
 	rm -rf output-*
-        PACKER_LOG=1 packer build -var-file=$img.secrets.pkrvars.hcl  $img.pkr.hcl
+        [[ -f $img.secrets.pkrvars.hcl ]] && PACKER_OPTS="$PACKER_OPTS -var-file=$img.secrets.pkrvars.hcl"
+        PACKER_LOG=1 packer build $PACKER_OPTS $img.pkr.hcl
         cd -
     }
 done
