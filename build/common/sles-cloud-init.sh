6ab1de206a98943546be9e4b8e43c2a7af34f0c4
Arnaud Veron <arnaud.veron@opensvc.com>
2024-04-09 09:27:29 +0200

    Fix typo in unit name

--- build/common/sles-cloud-init.sh
+++ build/common/sles-cloud-init.sh
@@ -4,6 +4,6 @@ echo "Installing Cloud Init"
 
 zypper --non-interactive --gpg-auto-import-keys install cloud-init
 systemctl enable cloud-init.service
-systemctl enable cloud-init-local.server
+systemctl enable cloud-init-local.service
 systemctl enable cloud-config.service
 systemctl enable cloud-final.service
