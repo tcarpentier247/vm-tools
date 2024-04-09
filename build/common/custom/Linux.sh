b0279d8075a9947f4d7501fae551ed9e983023ed
Arnaud Veron <arnaud.veron@opensvc.com>
2024-04-09 14:42:23 +0200

    Add grub reconfiguration

--- build/common/custom/Linux.sh
+++ build/common/custom/Linux.sh
@@ -3,6 +3,7 @@
 [[ -f /etc/default/grub ]] && {
     cp /etc/default/grub /etc/default/grub.osvcpacker
     sed -i 's#^GRUB_CMDLINE_LINUX_DEFAULT=.*$#GRUB_CMDLINE_LINUX_DEFAULT="console=tty0 console=ttyS0,115200n8"#' /etc/default/grub
+    update-grub || exit 1
 }
 
 exit 0
