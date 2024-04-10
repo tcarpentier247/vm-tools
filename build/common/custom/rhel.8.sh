fd909e613cec01eb439ddc6945bb57436030ecf7
Arnaud Veron <arnaud.veron@opensvc.com>
2024-04-10 16:57:33 +0200

    Fix rhel8 serial console

--- build/common/custom/rhel.8.sh
+++ build/common/custom/rhel.8.sh
@@ -9,6 +9,8 @@
 	    grub2-editenv - set "$kopts console=tty0 console=ttyS0,115200n8"
     }
     sed -i 's/GRUB_TERMINAL_OUTPUT="console"/GRUB_TERMINAL="console serial"/g' /etc/default/grub
+    # better to keep the line below
+    grep -q '^GRUB_CMDLINE_LINUX_DEFAULT' /etc/default/grub || echo 'GRUB_CMDLINE_LINUX_DEFAULT="console=tty0 console=ttyS0,115200n8"' >> /etc/default/grub
     grub2-mkconfig -o /boot/grub2/grub.cfg || exit 1
     grub2-mkconfig -o /boot/efi/EFI/redhat/grub.cfg || exit 1
 }
