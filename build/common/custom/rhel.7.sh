9581e491a692495ebbe7f2e432af14b3d2c6c7c6
Arnaud Veron <arnaud.veron@opensvc.com>
2024-04-10 15:51:26 +0200

    Fix rhel7 grub serial config

--- build/common/custom/rhel.7.sh
+++ build/common/custom/rhel.7.sh
@@ -5,6 +5,7 @@
     cp /etc/default/grub /etc/default/grub.osvcpacker
     # https://access.redhat.com/articles/7212
     sed -i 's#^GRUB_CMDLINE_LINUX_DEFAULT=.*$#GRUB_CMDLINE_LINUX_DEFAULT="console=tty0 console=ttyS0,115200n8"#' /etc/default/grub
+    grep -q '^GRUB_CMDLINE_LINUX_DEFAULT' /etc/default/grub || echo 'GRUB_CMDLINE_LINUX_DEFAULT="console=tty0 console=ttyS0,115200n8"' >> /etc/default/grub
     grep -q '^GRUB_TERMINAL=' /etc/default/grub || echo 'GRUB_TERMINAL="console serial"' >> /etc/default/grub
     grep -q '^GRUB_SERIAL_COMMAND=' /etc/default/grub || echo 'GRUB_SERIAL_COMMAND="serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1"' >> /etc/default/grub
     grub2-mkconfig -o /boot/grub2/grub.cfg || exit 1
