8affc433598a01d7f7fcced7275a7f489a004cd8
Arnaud Veron <arnaud.veron@opensvc.com>
2024-04-10 08:17:31 +0200

    Add rhel EFI grub config update

--- build/common/custom/rhel.sh
+++ build/common/custom/rhel.sh
@@ -5,6 +5,5 @@
     sed -i 's#^GRUB_CMDLINE_LINUX_DEFAULT=.*$#GRUB_CMDLINE_LINUX_DEFAULT="console=tty0 console=ttyS0,115200n8"#' /etc/default/grub
     grep -q '^GRUB_CMDLINE_LINUX_DEFAULT' /etc/default/grub || echo 'GRUB_CMDLINE_LINUX_DEFAULT="console=tty0 console=ttyS0,115200n8"' >> /etc/default/grub
     grub2-mkconfig -o /boot/grub2/grub.cfg || exit 1
+    grub2-mkconfig -o /boot/efi/EFI/redhat/grub.cfg || exit 1
 }
-
-exit 0
