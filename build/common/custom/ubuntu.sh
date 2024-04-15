a2c1c127d99aac8c34effda18adb56d3aba97a8b
Arnaud Veron <arnaud.veron@opensvc.com>
2024-04-15 07:58:03 +0200

    Enable ubuntu grub menu

--- build/common/custom/ubuntu.sh
+++ build/common/custom/ubuntu.sh
@@ -4,6 +4,8 @@
     cp /etc/default/grub /etc/default/grub.osvcpacker
     sed -i 's#^GRUB_CMDLINE_LINUX_DEFAULT=.*$#GRUB_CMDLINE_LINUX_DEFAULT="console=tty0 console=ttyS0,115200n8"#' /etc/default/grub
     grep -q '^GRUB_TERMINAL=' /etc/default/grub || echo 'GRUB_TERMINAL="console serial"' >> /etc/default/grub
+    sed -i 's/GRUB_TIMEOUT_STYLE=hidden/GRUB_TIMEOUT_STYLE=menu/' /etc/default/grub
+    sed -i 's/GRUB_TIMEOUT=0/GRUB_TIMEOUT=3/' /etc/default/grub
     update-grub || exit 1
 }
 
