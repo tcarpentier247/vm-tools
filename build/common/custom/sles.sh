84bbe11b6607f1a88b375f5816630fe4b0a5c9e2
Arnaud Veron <arnaud.veron@opensvc.com>
2024-04-26 14:09:11 +0200

    Add socat to sles base install

--- build/common/custom/sles.sh
+++ build/common/custom/sles.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-zypper --non-interactive --gpg-auto-import-keys install htop lsscsi
+zypper --non-interactive --gpg-auto-import-keys install htop lsscsi socat
 
 
 # serial console config
