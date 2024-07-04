c1b6b236d36cc5678b4d563a62a78be2fd832565
Arnaud Veron <arnaud.veron@opensvc.com>
2024-07-04 15:53:27 +0200

    Add podman defaut cni network

--- build/common/custom/rhel.sh
+++ build/common/custom/rhel.sh
@@ -1,3 +1,11 @@
 #/bin/bash
 
-echo
+[[ ! -d /var/lib/opensvc/cni/net.d ]] && mkdir -p /var/lib/opensvc/cni/net.d
+
+cat >| /var/lib/opensvc/cni/net.d/podman.conf <<EOF
+{
+    "cniVersion": "0.3.0",
+    "name": "podman",
+    "type": "loopback"
+}
+EOF
