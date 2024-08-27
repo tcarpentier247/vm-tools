484f4836f9fef976808a6b1da998497553dc7d8a
Arnaud Veron <arnaud.veron@opensvc.com>
2024-08-27 15:57:24 +0200

    Add clone or update

--- build/common/git-clone-vmtools.sh
+++ build/common/git-clone-vmtools.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-echo "Git cloning vm-tools repository to /opt/vm-tools"
-
 [[ -f /etc/profile ]] && . /etc/profile
 
-git clone https://github.com/opensvc/vm-tools.git /opt/vm-tools || exit 1
+if [ ! -d /opt/vm-tools ]
+then
+    echo "Git cloning https://github.com/opensvc/vm-tools.git to /opt/vm-tools"
+    git clone https://github.com/opensvc/vm-tools.git /opt/vm-tools || exit 1
+else
+    echo "Updating /opt/vm-tools git repository"
+    cd /opt/vm-tools
+    git pull --all || exit 1
+fi
+
