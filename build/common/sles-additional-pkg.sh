2bb5350d726b30c383fc09b994b4beacb90da101
Arnaud Veron <arnaud.veron@opensvc.com>
2024-09-04 08:37:35 +0200

    Add lxc to sles15

--- build/common/sles-additional-pkg.sh
+++ build/common/sles-additional-pkg.sh
@@ -6,7 +6,7 @@ zypper --non-interactive --gpg-auto-import-keys install qemu-kvm virt-install vi
 
 # only available on sles15
 grep -q 'suse:sles:15' /etc/os-release && {
-    zypper --non-interactive --gpg-auto-import-keys install mkisofs cni cni-plugins
+    zypper --non-interactive --gpg-auto-import-keys install mkisofs cni cni-plugins lxc lxc-bash-completion
 }
 
 exit 0
