babeceaa778fa208ae05a5bd44ca459a173b46be
Cyril Galibern <cyril.galibern@opensvc.com>
2024-09-11 11:48:35 +0200

    [sol114] update.sh change publisher to http://pkg.oracle.com/solaris/release
    1- text install image is defined to use the support repository, change it to public one.
    2- Add 1 retry when set-publisher fails

--- build/sol114/scripts/update.sh
+++ build/sol114/scripts/update.sh
@@ -6,6 +6,20 @@ echo "--- Begin update.sh ---"
 
 date; echo
 
+echo pkg set-publisher -G https://pkg.oracle.com/solaris/support/ -g http://pkg.oracle.com/solaris/release/ solaris
+pkg set-publisher -G https://pkg.oracle.com/solaris/support/ -g http://pkg.oracle.com/solaris/release/ solaris || {
+      echo "retry in 30s"
+      sleep 30
+      pkg set-publisher -G https://pkg.oracle.com/solaris/support/ -g http://pkg.oracle.com/solaris/release/ solaris || exit 1
+  }
+
+echo "pkg refresh"
+pkg refresh
+
+echo "pkg info entire"
+pkg info entire
+
+echo "pkg update --accept --no-backup-be"
 pkg update --accept --no-backup-be
 #pkg update --accept --no-backup-be pkg://solaris/entire@11.4-11.4.42.0.0.111.0
 
