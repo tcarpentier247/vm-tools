49dc3c55eb28d5a3141e2c4c5931e5e2738edde6
Arnaud Veron <arnaud.veron@opensvc.com>
2024-09-17 11:30:18 +0200

    Debug net.sh

--- build/common/cloud-init/net.sh
+++ build/common/cloud-init/net.sh
@@ -8,7 +8,10 @@ echo
 
 [[ -f ~opensvc/opensvc-qa.sh ]] && . ~opensvc/opensvc-qa.sh
 
+ls -l /nfs/data
+
 [[ -f /nfs/data/vdc.nodes.etc.hosts ]] && {
+    echo "Updating /etc/hosts"
     echo -e "\n# added by osvc build" >> /etc/hosts
     PATTERN=${HOSTNAME:0:6}
     grep $PATTERN /nfs/data/vdc.nodes.etc.hosts >> /etc/hosts
