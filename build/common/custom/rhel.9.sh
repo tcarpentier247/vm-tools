af145cb3718cd38e5c118076b8c60db89988eece
Arnaud Veron <arnaud.veron@opensvc.com>
2024-11-12 18:09:52 +0100

    Add EFI/BIOS test for grub rebuild

--- build/common/custom/rhel.9.sh
+++ build/common/custom/rhel.9.sh
@@ -7,6 +7,11 @@
     grubby --update-kernel=ALL --args="console=tty0 console=ttyS0,115200"
     grubby --info DEFAULT
     sed -i 's/GRUB_TERMINAL_OUTPUT="console"/GRUB_TERMINAL="console serial"/g' /etc/default/grub
-    grub2-mkconfig -o /boot/grub2/grub.cfg || exit 1
-    grub2-mkconfig -o /boot/efi/EFI/redhat/grub.cfg || exit 1
+
+    if test -d /sys/firmware/efi;
+    then
+        grub2-mkconfig -o /boot/efi/EFI/redhat/grub.cfg || exit 1
+    else
+        grub2-mkconfig -o /boot/grub2/grub.cfg || exit 1
+    fi
 }
