340170e1fb346833b23b10a10ac75f41e1b19955
Arnaud Veron <arnaud.veron@opensvc.com>
2025-01-27 11:07:36 +0100

    Add debian bullseye net config

--- build/common/debian-netplan.sh
+++ build/common/debian-netplan.sh
@@ -1,5 +1,13 @@
 #/bin/bash
 
+grep -qw bullseye /etc/os-release && {
+	systemctl unmask systemd-networkd.service
+	systemctl enable systemd-networkd.service
+	sed -i 's/#CONFIGURE_INTERFACES=yes/CONFIGURE_INTERFACES=no/' /etc/default/networking
+	rm -f /etc/network/interfaces
+	exit 0
+}
+
 export DEBIAN_FRONTEND=noninteractive
 
 apt install -y systemd-resolved netplan.io openvswitch-switch
