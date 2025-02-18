bda8450bf2a18b9aaa7ac6aa058ddf9cdffb8e34
Arnaud Veron <arnaud.veron@opensvc.com>
2025-02-18 08:09:25 +0100

    Add snap cleanup

--- build/common/sles-snapper.sh
+++ build/common/sles-snapper.sh
@@ -7,6 +7,15 @@ echo "Disabling Snapper"
 snapper -c root set-config "TIMELINE_CREATE=no"
 sed -i 's@USE_SNAPPER="yes"@USE_SNAPPER="no"@' /etc/sysconfig/yast2
 
+echo "snap list before cleanup"
+snapper --config root list
+
+LAST_SNAP_ID=$(snapper --config root list | tail -1 | awk '{print $1}')
+snapper --config root delete 2-$LAST_SNAP_ID
+
+echo "snap list after cleanup"
+snapper --config root list
+
 zypper --non-interactive remove snapper snapper-zypp-plugin grub2-snapper-plugin yast2-snapper-plugin
 
 exit 0
