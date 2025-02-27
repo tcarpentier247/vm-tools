e1b44ec9c67041a60967c05c7d189fe414951c49
Arnaud Veron <arnaud.veron@opensvc.com>
2025-02-27 11:03:11 +0100

    Add root disk auto resize

---
+++ build/common/cloud-init/disk_resize.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo
+echo "#############################"
+echo "######## DISK RESIZE ########"
+echo "#############################"
+echo
+
+[[ -f ~opensvc/opensvc-qa.sh ]] && . ~opensvc/opensvc-qa.sh
+
+[[ ! -e /dev/vda ]] && {
+	echo "error: /dev/vda not found"
+        exit 1
+}
+
+# fix error below
+# GPT PMBR size mismatch (20971519 != 62914559) will be corrected by write.
+# The backup GPT table is not on the end of the device.
+sgdisk -e /dev/vda
+
+# resize last partition
+(echo d; echo $(fdisk -l /dev/vda | grep -A1 '^/dev/vda' | tail -n 1 | awk '{print $1}' | sed 's/[^0-9]//g'); echo n; echo p; echo $(fdisk -l /dev/vda | grep -A1 '^/dev/vda' | tail -n 1 | awk '{print $1}' | sed 's/[^0-9]//g'); echo ; echo ; echo w) | fdisk /dev/vda && resize2fs /dev/vda$(fdisk -l /dev/vda | grep -A1 '^/dev/vda' | tail -n 1 | awk '{print $1}' | sed 's/[^0-9]//g')
+
+# resize lvm pv
+pvs -o name --noheadings | xargs -n1 pvresize
+
+exit 0
