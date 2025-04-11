4b4df6bce971a12ad451574e7098f7410a490599
Arnaud Veron <arnaud.veron@opensvc.com>
2025-04-11 10:45:26 +0200

    Add golang distrib

--- build/common/custom/Linux.sh
+++ build/common/custom/Linux.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
+function install_golang
+{
+    version=${1:-1.23.7}
+    url=https://dl.google.com/go/go$version.linux-amd64.tar.gz
+
+    echo "Installing golang $version"
+
+    basedir=/root/god-$version
+    echo mkdir $basedir
+    mkdir $basedir || exit 1
+
+    echo "download to $basedir: curl -s -o - $url | tar xzf -"
+    cd $basedir && curl -s -o - $url | tar xzf - || exit 1
+
+    echo "installing go and gofmt to /usr/bin"
+    ln -sf $basedir/go/bin/go /usr/bin/go || exit 1
+    ln -sf $basedir/go/bin/gofmt /usr/bin/gofmt || exit 1
+
+    /usr/bin/go version
+}
+
 [[ -x /opt/archives/docker/docker.restore.sh ]] && {
 	echo "Loading Docker/Podman images"
 	/opt/archives/docker/docker.restore.sh
 }
+
+install_golang
